f38ffbecc21f859d6905e70350f294ee2a78a12f
sergiomsantos <smf.dos.santos@gmail.com>
2018-10-02 17:10:49 +0100

    corrected bug in deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -20,7 +20,7 @@ makedocs(
 deploydocs(
     repo = "github.com/sergio-santos-group/ProtoSyn.jl.git",
     osname = "linux",
-    julia = "1.0"
+    julia = "1.0",
     deps = nothing,
     make = nothing,
     target = "build",
