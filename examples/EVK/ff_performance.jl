7319f68c211e2b4b620d50de4a6f64b59cad1529
sergiomsantos <smf.dos.santos@gmail.com>
2018-10-10 10:09:55 +0100

    added performance test

---
+++ examples/EVK/ff_performance.jl
@@ -0,0 +1,21 @@
+using ProtoSyn
+
+#Load state from PDB file
+state = Common.load_from_pdb("data/EVK.pdb")
+state.energy = Forcefield.Energy()
+
+#Load topologies from JSON files
+topol = Forcefield.load_from_json("data/EVK.json")
+
+function do_work(n::Int64)
+    for i = 1:n
+        fill!(state.forces, 0.0)
+        Forcefield.evaluate!(topol.bonds, state, do_forces=true)
+        Forcefield.evaluate!(topol.atoms, state, do_forces=true, cut_off=Inf)
+    end
+end
+
+# force JIT compilation
+do_work(10)
+
+@time do_work(5000)
