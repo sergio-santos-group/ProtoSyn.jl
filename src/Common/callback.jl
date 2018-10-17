b9a1d8fb6e8918f5de8487894a992d0a00e12c1c
JosePereiraUA <jose.manuel.pereira@ua.pt>
2018-10-17 17:19:20 +0100

    Added macros.jl

--- src/Common/callback.jl
+++ src/Common/callback.jl
@@ -15,7 +15,7 @@ Define the callback function parameters.
 julia> Common.CallbackObject(1, Print.as_xyz)
 CallbackObject(freq=1, callback=Print.as_xyz)
 
-See also: [`Print.as_xyz`](@ref Print)
+See also: [`Print.as_xyz`](@ref Print) [`cbcall`](@ref)
 ```
 """
 mutable struct CallbackObject
@@ -23,32 +23,3 @@ mutable struct CallbackObject
     callback::Function
 end
 Base.show(io::IO, b::CallbackObject) = print(io, "CallbackObject(freq=$(b.freq), callback=$(string(b.callback)))")
-
-@doc raw"""
-    @Common.cbcall callbacks::Tuple{CallbackObject, N} step::Int64 Vararg::Any
-
-(Macro) Call the CallbackObject.function of each [`CallbackObject`](@ref) in the `callbacks` Tuple{CallbackObject, N} independently.
-Each CallbackObject.function is ran depending on the defined CallbackObject.freq and the given `step`.
-Vararg holds all the arguments necessary to run the callback function itself.
-
-# Examples
-```julia-repl
-julia> @Common.cbcall (callback_object1, callback_object2) 1
-```
-"""
-macro cbcall(cbs, step, args...)
-    tmp = eval(cbs)
-
-    if length(tmp) == 0
-        ex = :nothing
-    else
-        ex = quote
-            for cb in $cbs
-                if (getproperty(cb, :freq)>0) && ($step%getproperty(cb, :freq)==0)
-                    getproperty(cb, :callback)($step, $(args...))
-                end
-            end
-        end
-    end
-    return esc(ex)
-end
---
+++ src/Common/macros.jl
@@ -0,0 +1,71 @@
+# ----------------------------------------------------------------------------------------------------------
+#                                                CALLBACK 
+@doc raw"""
+    @Common.cbcall callbacks::Tuple{CallbackObject, N} step::Int64 Vararg::Any
+
+(Macro) Call the CallbackObject.function of each [`CallbackObject`](@ref) in the `callbacks` Tuple{CallbackObject, N} independently.
+Each CallbackObject.function is ran depending on the defined CallbackObject.freq and the given `step`.
+Vararg holds all the arguments necessary to run the callback function itself.
+
+# Examples
+```julia-repl
+julia> @Common.cbcall (callback_object1, callback_object2) 1
+```
+"""
+macro cbcall(cbs, step, args...)
+    tmp = eval(cbs)
+
+    if length(tmp) == 0
+        ex = :nothing
+    else
+        ex = quote
+            for cb in $cbs
+                if (getproperty(cb, :freq)>0) && ($step%getproperty(cb, :freq)==0)
+                    getproperty(cb, :callback)($step, $(args...))
+                end
+            end
+        end
+    end
+    return esc(ex)
+end
+
+# ----------------------------------------------------------------------------------------------------------
+#                                                Energy
+@doc raw"""
+    @Common.faggregator name::String f::function Vararg::Any
+
+(Macro) Aggregate multiple functions `f` in a single variable `name`. `Vararg` contains the arguments used by function `f`
+
+# Examples
+```julia-repl
+julia> @faggregator myeval f top1
+@faggregator myeval g top2
+@faggregator myeval h top3
+
+energy = myevalf(state, false)
+```
+"""
+macro faggregator(name, f, args...)
+    cachename = Symbol("##", "_faggregator_cache")
+    mod = @__MODULE__
+    
+    cache = isdefined(mod, cachename) ?
+             getfield(mod, cachename) :
+             Core.eval(mod, :(const $cachename = Dict{Symbol, Expr}()))
+
+    if haskey(cache, name, )
+        ex = :(energy += $f($(args...), s, b))
+        expr = cache[name]
+        pos = length(expr.args[2].args[2].args) - 2
+        insert!(expr.args[2].args[2].args, pos, ex)
+    else
+        cache[name] = quote
+            function $name(s::State, b::Bool)
+                energy = $f($(args...), s, b)
+                s.eTotal = energy
+                return energy
+            end
+        end
+    end
+    esc(cache[name])
+end
