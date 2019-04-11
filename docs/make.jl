3d2026a9578a38a2035bda1b0f8521a9a659b451
JosePereiraUA <jose.manuel.pereira@ua.pt>
2019-04-11 16:04:50 +0100

    Fixed Documentation organization

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,15 @@
 using Documenter, ProtoSyn
-makedocs(sitename = "ProtoSyn")
+makedocs(
+    sitename = "ProtoSyn",
+    pages = [
+        "Home" => "index.md",
+        "Manual" => [
+            "Common" => "common.md",
+            "Forcefield" => "forcefield.md",
+            "Mutators" => "mutators.md",
+            "Drivers" => "drivers.md",
+            "Print" => "print.md",
+            "Aux" => "aux.md"
+        ],
+])
 deploydocs(repo = "github.com/JosePereiraUA/ProtoSyn.jl.git")
