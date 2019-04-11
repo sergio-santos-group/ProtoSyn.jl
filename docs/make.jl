e1e1924c8650ee83f291e54a0434c1ff69a9bcf9
JosePereiraUA <jose.manuel.pereira@ua.pt>
2019-04-11 14:29:46 +0100

    Cleaned Documentation deployment

--- docs/make.jl
+++ docs/make.jl
@@ -1,37 +1,3 @@
-# if Base.HOME_PROJECT[] !== nothing
-#     Base.HOME_PROJECT[] = abspath(Base.HOME_PROJECT[])
-#   end
-
-# using Documenter, ProtoSyn
-
-# push!(LOAD_PATH,"../src/")
-
-# makedocs(
-#     format = :html,
-#     sitename = "ProtoSyn.jl",
-#     pages = [
-#         "Home" => "index.md",
-#         # "Guide" => "guide.md",
-#         "Manual" => [
-#             "Common" => "common.md",
-#             "Forcefield" => "forcefield.md",
-#             "Mutators" => "mutators.md",
-#             "Drivers" => "drivers.md",
-#             "Print" => "print.md",
-#             "Aux" => "aux.md",
-#             "Input JSON Schemas" => "json.md"
-#         ],
-#     ]
-# )
-
-
 using Documenter, ProtoSyn
-
-makedocs(sitename = "Test")
-
-deploydocs(
-    repo = "github.com/JosePereiraUA/ProtoSyn.jl.git",
-    deps = nothing,
-    make = nothing,
-    target = "build",
-)
+makedocs(sitename = "ProtoSyn")
+deploydocs(repo = "github.com/JosePereiraUA/ProtoSyn.jl.git")
