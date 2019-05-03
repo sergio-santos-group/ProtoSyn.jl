3d17dafd48d1a7b57468f5bd16482348eea96d85
sergiomsantos <smf.dos.santos@gmail.com>
2019-05-03 15:15:00 +0100

    nonbonded list stuff

--- src/Common/state.jl
+++ src/Common/state.jl
@@ -1,6 +1,3 @@
-# ----------------------------------------------------------------------------------------------------------
-#                                                 STATE
-
 mutable struct NonBondedList
     cutoff::Float64
     buffer::Float64
@@ -8,25 +5,37 @@ mutable struct NonBondedList
     pointer::Vector{Int64}
 end
 
-NonBondedList(n::Int64) = NonBondedList(zeros(Int64, convert(Int64, n*(n+1)/2)), zeros(n))
-Base.show(io::IO, b::NonBondedList) = print(io, "NonBondedList(list=$(b.list), pointer=$(b.pointer))")
+function NonBondedList(n::Int)
+    list_size = convert(Int, n*(n+1)/2)
+    NonBondedList(-1.0, 0.0, zeros(Int, list_size), zeros(n))
+end
 
-Base.copy!(dst::NonBondedList, src::NonBondedList)::NonBondedList = begin
-    copy!(dst.list, src.list)
+function Base.show(io::IO, b::NonBondedList)
+    print(io, "NonBondedList(list=$(b.list), pointer=$(b.pointer))")
+    print(io, "\n   pointer size = $(length(b.pointer)) items")
+    print(io, "\n   list size = $(length(b.list)) items")
+    print(io, "\n   cutoff = $(b.cutoff) nm")
+    print(io, "\n   buffer = $(b.buffer) nm")
+end
+ 
+function Base.copy!(dst::NonBondedList, src::NonBondedList)
     copy!(dst.pointer, src.pointer)
+    copy!(dst.list, src.list)
+    dst.cutoff = src.cutoff
+    dst.buffer = src.buffer
     return dst
 end
 
 
 @doc raw"""
-    State(size::Int64, energy::AbstractEnergy, xyz::Array{Float64, 2}, forces::Array{Float64, 2}, metadata::Vector{Metadata})
+    State(size::Int64, energy::Energy, xyz::Array{Float64, 2}, forces::Array{Float64, 2}, metadata::Vector{Metadata})
 
 Define the current state of the system, containing the atoms positions, energy and forces applied.
 If only `size::Int64` is provided, an empty State with the given `size` is created with zeros.
 
 # Arguments
 - `size::Int64`: Atom count in system.
-- `energy::AbstractEnergy`: Current energy of the system (kJ mol⁻¹).
+- `energy::Energy`: Current energy of the system (kJ mol⁻¹).
 - `xyz::Array{Float64, 2}`: Atom positions in 3 dimensions.
 - `forces::Array{Float64, 2}`: Forces applied in each dimension to each atom (kJ mol⁻¹ nm⁻¹)
 - `metadata::Metadata`: Metadata object.
@@ -42,19 +51,20 @@ Common.State(size=2, energy=Null, xyz=[1.1 1.1 1.1; 2.2 2.2 2.2], forces=[0.0 0.
 See also: [`Metadata`](@ref)
 """
 mutable struct State
-
     size::Int64
-    energy::AbstractEnergy
+    energy::Energy
     xyz::Array{Float64, 2}
     forces::Array{Float64, 2} # kJ mol⁻¹ nm⁻¹
     nblist::Union{NonBondedList, Nothing}
 end
-State(n::Int64, e::AbstractEnergy, x::Array{Float64, 2}, f::Array{Float64, 2}) = State(n, e, x, f, NonBondedList(n))
+State(n::Int64, e::Energy, x::Array{Float64, 2}, f::Array{Float64, 2}) = State(n, e, x, f, NonBondedList(n))
 State(n::Int64) = State(n, Energy(), zeros(n, 3), zeros(n, 3), NonBondedList(n))
-State(state::State) = begin
+
+function State(state::State)
     st = State(state.size)
     copy!(st, state)
 end
+
 Base.show(io::IO, b::State) = print(io, "State(size=$(b.size), energy=$(b.energy), xyz=$(b.xyz), forces=$(b.forces), nb=$(b.nb)")
 
 function Base.iterate(st::State, idx = 1)
@@ -74,32 +84,32 @@ function Base.copy!(dst::State, src::State)::State
     return dst
 end
 
-function update_nblist!(state::Common.State)
-    #! `top` type is defined as Any, but it should be Forcefield.Amber.Topology.
-    # The issue is that so far, ProtoSyn does not know that Forcefield is!
 
+
+
+function update_nblist!(state::Common.State)
+    # if no NonBondedList is attached to the input state,
+    # the do nothing
     if state.nblist == nothing
         return state
     end
+    
+    nblist  = state.nblist
+    n_atoms = state.size
+    coords  = state.xyz
 
-
-    # ptr         = 1
-    # exclude_idx = 1
-    # exclude     = 1
-    # dijSq       = 0.0
-    # delta       = 0.0
-    # cutSq       = state.nblist.cutoff ^ 2
-    coords      = state.xyz
-    n_atoms     = state.size
-
-    nblist = state.nblist
     if nblist.cutoff < 0.0
+        # if all pairwise interactions are requested
+        # (cutoff < 0), construction of the nblist does
+        # not require any distance computations
         for i=1:n_atoms
             nblist.pointer[i] = i
             nblist.list[i] = i+1
         end
         nblist.list[n_atoms] = -1
     else
+        # otherwise, the effective cutoff is the
+        # sum of the cuttoff and the buffer.
         ptr = 1
         δx = 0.0
         Δx = 0.0
@@ -120,47 +130,7 @@ function update_nblist!(state::Common.State)
             nblist.list[ptr] = -1
             ptr += 1
         end
-    end
-
-    # for i=1:n_atoms
-        
-    #     # # set the exclution index to the correct location and extract the exclude atom index
-    #     # @inbounds atomi = top.atoms[i]
-    #     # len_exclusions = length(atomi.excls)
-    #     # if len_exclusions > 0
-    #     #     exclude_idx = 1
-    #     #     @inbounds while atomi.excls[exclude_idx] <= i && exclude_idx < len_exclusions
-    #     #         exclude_idx += 1
-    #     #     end
-    #     #     @inbounds exclude = atomi.excls[exclude_idx]
-    #     # end
-
-    #     state.nb.pointer[i] = ptr
-
-    #     for j = (i+1):n_atoms
-
-    #         # # Remove excluded atom
-    #         # if j == exclude
-    #         #     exclude_idx += 1
-    #         #     @inbounds exclude = atomi.excls[exclude_idx]
-    #         #     continue
-    #         # end
-
-    #         dijSq = 0.0
-    #         for k=1:3
-    #             delta = coords[i,k] - coords[j,k]
-    #             dijSq += delta*delta
-    #         end
-
-    #         if dijSq < cutSq
-    #             state.nb.list[ptr] = j
-    #             ptr += 1
-    #         end
-    #     end
-        
-    #     state.nb.list[ptr] = -1
-    #     ptr += 1
-    # end
+    end # endif
 
     return state
-end
+end # end fcn
