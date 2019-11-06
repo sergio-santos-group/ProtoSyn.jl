3af28dd486016f3a22cf121807186378220df296
Sergio M. Santos <smf.dos.santos@gmail.com>
2019-11-06 14:26:39 -0500

    removed all files

--- base/data/workshop/native2straight.jl
+++
@@ -1,8 +0,0 @@
-using ProtoSyn
-
-output_file = open("1i2t.pdb", "w")
-state, metadata = Common.load_from_pdb("1i2t_native.pdb")
-backbone = filter(x -> x.dtype <= Common.DIHEDRAL.omega, metadata.dihedrals)
-Common.stretch_conformation!(state, backbone)
-Print.as_pdb(output_file, state, metadata)
-close(output_file)
--- src/Aux/Aux.jl
+++
@@ -1,9 +0,0 @@
-module Aux
-
-using LinearAlgebra
-using JSON
-
-include("io.jl")
-include("misc.jl")
-
-end
