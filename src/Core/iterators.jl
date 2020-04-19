5155ce03b307de9a3c11e9e8af0959206efb5e05
Sergio M. Santos <smf.dos.santos@gmail.com>
2020-04-19 21:33:42 +0100

    added byresidue iterator

--- src/Core/iterators.jl
+++ src/Core/iterators.jl
@@ -1,16 +1,18 @@
 export eachatom, eachresidue
 
 #region Iterators --------------------------------------------------------------
-const _ByAtom = Val{1}
-const _ByResidue = Val{2}
+# const _ByAtom = Val{1}
+# const _ByResidue = Val{2}
+struct _ByAtom end
+struct _ByResidue end
 
 struct ItemIterator{T <: AbstractContainer,B}
     target::T
     size::Tuple{Vararg{Int}}
 end
 
-Base.length(iter::ItemIterator{T,B}) where {T,B} = iter.size[end]
 Base.size(iter::ItemIterator{T,B}) where {T,B} = iter.size
+Base.length(iter::ItemIterator{T,B}) where {T,B} = iter.size[end]
 
 Base.show(io::IO,iter::ItemIterator{T,_ByAtom}) where T =
     println(io, "ItemIterator{$(nameof(T)), _ByAtom} with size $(iter.size)")
@@ -80,6 +82,15 @@ Base.iterate(iter::ItemIterator{Topology,_ByResidue}, (s,r)=(1,1)) = begin
     end
     (t.items[s].items[r], (s,r+1))
 end
+
+Base.iterate(iter::ItemIterator{Segment,_ByResidue}, (r,)=(1,)) = begin
+    s = iter.target
+    if r > length(s)
+        return nothing
+    end
+    (s.items[r], (r+1,))
+end
+
 #region ResidueIterator
 
 
