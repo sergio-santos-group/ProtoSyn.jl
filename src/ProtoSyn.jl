1597d3e2c8d5fb732a3ab68a27705e37252c9089
Sergio M. Santos <smf.dos.santos@gmail.com>
2020-05-12 09:37:18 +0100

    added pose as Graph+State packing strcut

--- src/ProtoSyn.jl
+++ src/ProtoSyn.jl
@@ -25,8 +25,20 @@ include("XMLRPC/XMLRPC.jl")
 include("core/graph.jl")
 include("core/macros.jl")
 include("core/types.jl")
-include("core/base.jl")
 include("core/state.jl")
+
+export Pose
+struct Pose{T<:AbstractContainer}
+    graph::T
+    state::State
+    Pose(c::T, s::State) where {T<:AbstractContainer}= begin
+        c.id != s.id && error("unpairable container and state")
+        new{T}(c,s)
+    end
+end
+
+include("core/base.jl")
+
 
 include("core/selection2.jl")
 
@@ -49,20 +61,21 @@ include("core/selection2.jl")
 #     topmost && hascontainer(c) ? container(c.container; topmost=true) : c.container
 # end
 
-
+const Fragment = Pose{Segment}
 export ResidueDB
-const ResidueDB = Dict{String, Tuple{Residue, State}}
+const ResidueDB = Dict{String, Fragment}
 
 include("core/io.jl")           # <-- ATTENTION
 include("core/iterators.jl")    # <-- ATTENTION
 include("core/methods.jl")
 
 
-export from
-from(db::ResidueDB, key::String) = begin
-    r,s=db[key]
-    copy(r), copy(s)
-end
+#export from
+#from(db::ResidueDB, key::String) = begin
+#    r,s=db[key]
+#
+#    copy(r), copy(s)
+#end
 
 #include("core/state2.jl")
 #include("core/base2.jl")
