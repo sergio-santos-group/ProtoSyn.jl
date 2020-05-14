d2ecd43db09753ace9089299b1204cc2f05bed31
Sergio M. Santos <smf.dos.santos@gmail.com>
2020-05-14 21:59:06 +0100

    removed blank line

--- src/Sugars/Sugars.jl
+++ src/Sugars/Sugars.jl
@@ -2,6 +2,7 @@ module Sugars
 
 using ..ProtoSyn
 
+# resource directory for this module
 const resource_dir = let
     modname = string(nameof(@__MODULE__))
     joinpath(ProtoSyn.resource_dir, modname)
