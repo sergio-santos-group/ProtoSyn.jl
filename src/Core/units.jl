2edfec6c1f2729baa5fe4ff875cba30367a46088
Sergio M. Santos <smf.dos.santos@gmail.com>
2020-05-26 11:53:08 +0100

    added meter

--- src/Core/units.jl
+++ src/Core/units.jl
@@ -1,12 +1,14 @@
 module Units
 
-export °, Å, nm, tonumber
+export °, Å, nm, m, tonumber
 
 const ° = deg2rad(1)
 const Å = 1
 const nm = 10Å
+const m = 1e10Å
 
 tonumber(v::Number) = v
+# tonumber(v::String) = (x=eval(Meta.parse(v)); println(v,"=",x); x)
 tonumber(v::String) = eval(Meta.parse(v))
 
 end
