253fc7fc7cea72b7865a694323f61c0fa612cfd7
Sergio M. Santos <smf.dos.santos@gmail.com>
2020-06-03 17:59:52 +0100

    bonds, angles, proper, improper eval for amber03

--- src/Calculators/Forcefield/bonded.jl
+++ src/Calculators/Forcefield/bonded.jl
@@ -1,7 +1,19 @@
 using Base.Cartesian
 import ..Calculators.eval!
 
-@generated function eval!(state::State{T}, bonds::Vector{HarmonicPotential{Int,2,T}}, ::Type{Val{DoF}}) where {T<:AbstractFloat, DoF}
+@generated function eval!(state::State{T}, ff::Forcefield2, ::Type{Val{DoF}}) where {T<:AbstractFloat, DoF}
+quote
+    reset(state)
+    for (k,v) in ff.components
+        eval!(state, v, Val{DoF})
+    end
+    state
+end
+end
+
+
+
+@generated function eval!(state::State{T}, bonds::Vector{HarmonicBond{Int,T}}, ::Type{Val{DoF}}) where {T<:AbstractFloat, DoF}
 println("generating for $DoF, $T")
 quote
     energy = $(T(0))
@@ -12,7 +24,7 @@ quote
         a2 = bond.key[2]
         
         @nexprs 3 u -> v12_u = coords[a2,u] - coords[a1,u]
-        d12 = sqrt(ProtoSyn.@dot(u, v12_u, v12_u))
+        d12 = sqrt(@dot(u, v12_u, v12_u))
         dr = d12 - bond.x0
 
         energy += bond.kf * dr * dr
@@ -28,7 +40,7 @@ quote
         #endregion
 
     end
-    state.e[HarmonicPotential{Int,2,T}] = energy/2
+    state.e[HarmonicBond{Int,T}] = energy/2
     state
 end
 end
@@ -36,7 +48,7 @@ end
 
 
 
-@generated function eval!(state::State{T}, angles::Vector{HarmonicPotential{Int,3,T}}, ::Type{Val{DoF}}) where {T<:AbstractFloat, DoF}
+@generated function eval!(state::State{T}, angles::Vector{HarmonicAngle{Int,T}}, ::Type{Val{DoF}}) where {T<:AbstractFloat, DoF}
 quote
     energy = $(T(0))
     coords = state.x
@@ -68,7 +80,7 @@ quote
             quote
                 # prevent division by zero
                 fc = max(1.0 - ctheta*ctheta, 1e-8)
-                fc = dtheta * type.kf / sqrt(fc)
+                fc = dtheta * angle.kf / sqrt(fc)
                 
                 @nexprs 3 u -> begin
                     f1 = fc*(ctheta * v21_u*inv_d21sq - v23_u*inv_d21xd23)
@@ -82,8 +94,135 @@ quote
         #endregion
             
     end
-    state.e[HarmonicPotential{Int,3,T}] = energy/2
+    state.e[HarmonicAngle{Int,T}] = energy/2
     state
 end
 end
 
+
+
+
+
+@generated function eval!(state::State{T}, dihedrals::Vector{CosineDihedral{Int,T}}, ::Type{Val{DoF}}) where {T<:AbstractFloat, DoF}
+quote
+    energy = $(T(0))
+    coords = state.x
+    forces = state.f
+    @inbounds for dihd in dihedrals
+
+        a1,a2,a3,a4 = dihd.key
+        
+        @nexprs 3 u -> v12_u = coords[a2,u] - coords[a1,u]
+        @nexprs 3 u -> v32_u = coords[a2,u] - coords[a3,u]
+        @nexprs 3 u -> v34_u = coords[a4,u] - coords[a3,u]
+        
+        d32sq = @dot u v32_u v32_u
+        d32 = sqrt(d32sq)
+        
+        @cross u vm_u v12_u v32_u   # v12 × v32
+        @cross u vn_u v32_u v34_u   # v32 × v34
+        d12n = @dot u v12_u vn_u    # v12⋅vn
+        dmn  = @dot u vm_u vn_u     # vm⋅vn
+
+        phi = atan(d32 * d12n, dmn)
+        energy += dihd.k * (1.0 + cos(dihd.n * phi - dihd.θ))
+        
+        #region FORCE_SECTION
+        $(if DoF === true
+            quote
+                
+                dVdphi_x_d32 = dihd.k * dihd.n * sin(dihd.θ - dihd.n * phi) * d32
+                d3432 = @dot(u, v34_u, v32_u)   # v34⋅v32
+                d1232 = @dot(u, v12_u, v32_u)   # v12⋅v32
+                dmm = @dot(u, vm_u, vm_u)       # vm⋅vm
+                dnn = @dot(u, vn_u, vn_u)       # vn⋅vn
+                
+                c3_1 = (d3432/d32sq - 1.0)
+                c3_2 = (d1232/d32sq)
+
+                c1   = -dVdphi_x_d32 / dmm
+                c4   =  dVdphi_x_d32 / dnn
+
+                @nexprs 3 u -> begin
+                    f1 = vm_u * c1
+                    f4 = vn_u * c4
+                    f3 = f4 * c3_1 - f1 * c3_2
+                    forces[a1, u] -= f1
+                    forces[a2, u] -= (-f1 - f3 - f4)
+                    forces[a3, u] -= f3
+                    forces[a4, u] -= f4
+                end
+            end
+        end)
+        #endregion
+
+    end
+    state.e[CosineDihedral{Int,T}] = energy
+    state
+end
+end
+
+
+
+@generated function eval!(state::State{T}, dihedrals::Vector{NCosineDihedral{Int,T}}, ::Type{Val{DoF}}) where {T<:AbstractFloat, DoF}
+    quote
+        energy = $(T(0))
+        coords = state.x
+        forces = state.f
+        @inbounds for dihd in dihedrals
+    
+            a1,a2,a3,a4 = dihd.key
+            
+            @nexprs 3 u -> v12_u = coords[a2,u] - coords[a1,u]
+            @nexprs 3 u -> v32_u = coords[a2,u] - coords[a3,u]
+            @nexprs 3 u -> v34_u = coords[a4,u] - coords[a3,u]
+            
+            d32sq = @dot u v32_u v32_u
+            d32 = sqrt(d32sq)
+            
+            @cross u vm_u v12_u v32_u   # v12 × v32
+            @cross u vn_u v32_u v34_u   # v32 × v34
+            d12n = @dot u v12_u vn_u    # v12⋅vn
+            dmn  = @dot u vm_u vn_u     # vm⋅vn
+    
+            phi = atan(d32 * d12n, dmn)
+            for i in eachindex(dihd.k)
+                energy += dihd.k[i] * (1.0 + cos(dihd.n[i] * phi - dihd.θ[i]))
+            end
+            
+            #region FORCE_SECTION
+            $(if DoF === true
+                quote
+                    
+                    d3432 = @dot(u, v34_u, v32_u)   # v34⋅v32
+                    d1232 = @dot(u, v12_u, v32_u)   # v12⋅v32
+                    dmm = @dot(u, vm_u, vm_u)       # vm⋅vm
+                    dnn = @dot(u, vn_u, vn_u)       # vn⋅vn
+                    
+                    c3_1 = (d3432/d32sq - 1.0)
+                    c3_2 = (d1232/d32sq)
+                    
+                    for i in eachindex(dihd.k)
+                        dVdphi_x_d32 = dihd.k[i] * dihd.n[i] * sin(dihd.θ[i] - dihd.n[i] * phi) * d32
+                        c1   = -dVdphi_x_d32 / dmm
+                        c4   =  dVdphi_x_d32 / dnn
+        
+                        @nexprs 3 u -> begin
+                            f1 = vm_u * c1
+                            f4 = vn_u * c4
+                            f3 = f4 * c3_1 - f1 * c3_2
+                            forces[a1, u] -= f1
+                            forces[a2, u] -= (-f1 - f3 - f4)
+                            forces[a3, u] -= f3
+                            forces[a4, u] -= f4
+                        end
+                    end
+                end
+            end)
+            #endregion
+    
+        end
+        state.e[NCosineDihedral{Int,T}] = energy
+        state
+    end
+    end
