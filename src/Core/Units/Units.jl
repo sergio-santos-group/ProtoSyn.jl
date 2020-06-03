0155c1bf0796e93cc16cdbf8c56a1478bbae10fb
Sergio M. Santos <smf.dos.santos@gmail.com>
2020-06-03 17:58:45 +0100

    added new units

--- src/Core/Units/Units.jl
+++ src/Core/Units/Units.jl
@@ -1,14 +1,19 @@
 module Units
 
-export °, Å, nm, m, tonumber
+export °, Å, kJ, mol, rad, nm, m, J, tonumber
 
 const ° = deg2rad(1)
-const Å = 1
+const   Å = 1
+const  kJ = 1
+const mol = 1
+const rad = 1
+
 const nm = 10Å
 const m = 1e10Å
+const J = kJ/1000
 
 tonumber(v::Number) = v
 # tonumber(v::String) = (x=eval(Meta.parse(v)); println(v,"=",x); x)
 tonumber(v::String) = eval(Meta.parse(v))
-
+kJ/mol/nm^2
 end
