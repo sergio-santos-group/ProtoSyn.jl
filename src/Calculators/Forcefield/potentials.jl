81b5637ed0b7f684e565c9075880f26f236ccffb
Sergio M. Santos <smf.dos.santos@gmail.com>
2020-06-03 18:14:27 +0100

    concrete function for all potentials

--- src/Calculators/Forcefield/potentials.jl
+++ src/Calculators/Forcefield/potentials.jl
@@ -10,8 +10,8 @@ end
 
 struct HarmonicPotential{K, N, T<:AbstractFloat} <: AbstractPotential
     key::KeyType{K,N}
-    kf::T
     x0::T
+    kf::T
 end
 
 struct CosinePotential{K, N, T<:AbstractFloat} <: AbstractPotential
@@ -27,6 +27,9 @@ struct NCosinePotential{K, N, T<:AbstractFloat} <: AbstractPotential
     k::Tuple{Vararg{T}}
     n::Tuple{Vararg{T}}
 end
+NCosinePotential{K, N, T}(key::KeyType{K,N}, θ::Number, k::Number, n::Number) where {K,N,T<:AbstractFloat} = begin
+    NCosinePotential{K, N, T}(key, (θ,), (k,), (n,))
+end
 
 const CoulombLennardJones{K,T} = CoulombLennardJonesPotential{K,1,T} where {K,T}
 const    HarmonicBond{K,T} = HarmonicPotential{K,2,T} where {K,T}
@@ -34,10 +37,22 @@ const   HarmonicAngle{K,T} = HarmonicPotential{K,3,T} where {K,T}
 const  CosineDihedral{K,T} =   CosinePotential{K,4,T} where {K,T}
 const NCosineDihedral{K,T} =  NCosinePotential{K,4,T} where {K,T}
 
-concrete(x::HarmonicPotential{K,N,T}, k::KeyType{W,N}) where {K,N,T,W}= begin
-    HarmonicPotential{W,N,T}(k, x.kf, x.x0)
-end
-
 concrete(x::CoulombLennardJonesPotential{K,N,T}, k::KeyType{W,N}, q::T) where {K,N,T,W}= begin
     CoulombLennardJonesPotential{W,N,T}(k, x.z, x.m, q, x.σ, x.ϵ)
+end
+
+concrete(x::HarmonicPotential{K,N,T}, k::KeyType{W,N}) where {K,N,T,W}= begin
+    HarmonicPotential{W,N,T}(k, x.x0, x.kf)
+end
+concrete(x::CosinePotential{K,N,T}, k::KeyType{W,N}) where {K,N,T,W}= begin
+    CosinePotential{W,N,T}(k, x.θ, x.k, x.n)
+end
+concrete(x::NCosinePotential{K,N,T}, k::KeyType{W,N}) where {K,N,T,W}= begin
+    NCosinePotential{W,N,T}(k, x.θ, x.k, x.n)
+end
+
+
+
+Base.:+(p1::NCosinePotential{K,N,T}, p2::NCosinePotential{K,N,T}) where {K,N,T} = begin
+    NCosinePotential{K,N,T}(p1.key, (p1.θ...,p2.θ...), (p1.k...,p2.k...), (p1.n...,p2.n...))
 end
