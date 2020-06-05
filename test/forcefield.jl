3cab79527fe2d54524981010db21bb8d8a7a751e
Sergio M. Santos <smf.dos.santos@gmail.com>
2020-06-05 14:52:57 +0100

    updated test

--- test/forcefield.jl
+++ test/forcefield.jl
@@ -1,75 +1,69 @@
-# module Tst
-
-# include("../src/Calculators/Forcefield/Forcefield.jl")
-# using .Forcefield
-
-# ff = loadparm("resources/Calculators/Forcefield/protoff.yml")
-
-# end
-
-
 module Tst
 
 include("../src/ProtoSyn.jl")
 
+using YAML
 using .ProtoSyn
 using .ProtoSyn.Builder
 using .ProtoSyn.Peptides
 using .ProtoSyn.Calculators.Forcefield
 
 grammar = Peptides.grammar(Float64)
-pose = build(grammar, seq"AA")
+pose = build(grammar, seq"AAAAAAAAAA")
 
-# setss!(pose, SecondaryStructure[:linear])
-# @pymol sync!(pose)
+setss!(pose, SecondaryStructure[:linear])
+@pymol sync!(pose)
 
-ff = ForcefieldParameters("resources/Calculators/Forcefield/protoff.yml")
-blist = genbonded(pose.graph,3)
-
-using YAML
+ff = ForcefieldParameters("resources/Calculators/Forcefield/forcefield.yml")
 resmaps = YAML.load_file("resources/Calculators/Forcefield/aminoacids.yml")
-# atypes = assigntypes(pose.graph[1], resmaps)
+# blist = genbonded(pose.graph,3)
 
-ftop = genff(pose.graph[1], ff, resmaps)
+
+ftop = genff(pose.graph, ff, resmaps)
 state = Forcefield.State(pose.state)
 
 const   FORCES = Val{true}
 const NOFORCES = Val{false}
 
-# ProtoSyn.Calculators.eval!(
-#     state,
-#     ftop.components[Forcefield.HarmonicPotential{Int64,2,Float64}],
-#     FORCES
-#     )
-# ProtoSyn.Calculators.eval!(
-#     state,
-#     ftop.components[Forcefield.HarmonicPotential{Int64,2,Float64}],
-#     NOFORCES
-#     )
-
 @noinline function f1(n::Int, dof)
-    # e = 0.0
-    # for i=1:n
-    #     e += i
-    # end
-    bonds = ftop.components[Forcefield.HarmonicPotential{Int64,2,Float64}]
-    #println(typeof(bonds))
     for i=1:n
-        #e += 
-        ProtoSyn.Calculators.eval!(state, bonds, dof)
+        ProtoSyn.Calculators.eval!(state, ftop, dof)
     end
-    
+    println(Forcefield.energy(state))
 end
 
-# f1(10,   FORCES)
-# f1(10, NOFORCES)
-# f1(100000,   FORCES)
-# f1(100000, NOFORCES)
-# f(n,f) = @time f1(n,f)
-
-@time f1(    1,   FORCES)
 @time f1(    1, NOFORCES)
-@time f1(100000,   FORCES)
-@time f1(100000, NOFORCES)
+@time f1(    1,   FORCES)
+@time f1(1000, NOFORCES)
+@time f1(1000,   FORCES)
+
+function sd(n)
+    λ = 0.00005
+    eOld = Inf64
+
+    for i=1:n
+        ProtoSyn.Calculators.eval!(state, ftop, FORCES)
+        eNew = Forcefield.energy(state)
+        copy!(pose.state, state)
+        
+        if i%5000==0
+            println("Step ", i, " E = ", eNew, " with λ=", λ)
+            for (k,v) in state.e
+                # println("   ", name(k),"{", k.parameters[2],"} = ", v)
+                println("   ", name(k), " = ", v)
+            end
+            @pymol pose
+        end
+        @. state.x += λ*state.f
+        if eNew < eOld
+            λ*=1.05
+        else
+            λ*=0.5
+        end
+        eOld = eNew
+
+    end
+end
+# sd(1000_000)
 
 end
