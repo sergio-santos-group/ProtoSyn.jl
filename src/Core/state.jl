f37a61d2f9064c8c2f1b4e310e47e033b86802a0
Sergio M. Santos <smf.dos.santos@gmail.com>
2020-06-12 22:53:31 +0100

    added coordinate representation

--- src/Core/state.jl
+++ src/Core/state.jl
@@ -63,9 +63,11 @@ mutable struct State{T<:AbstractFloat}
     c2i::Bool           # flag to request cartesian to internal conversion
     index_offset::Int   # ...
 
-    # x::Array{T,2}
+    x::Opt{Matrix{T}}
+    f::Opt{Matrix{T}}
     # f::Array{T,2}
     # v::Array{T,2}
+    e::Dict{Symbol,T}
 end
 
 State{T}(n::Int) where T = begin
@@ -76,7 +78,8 @@ State{T}(n::Int) where T = begin
     items[1].t[1] = -1.0
     items[1].t[2] =  1.0
     items[2].t[1] = -1.0
-    State{T}(items, n, -1, false, false, 3)
+    # State{T}(items, n, -1, false, false, 3)
+    State{T}(items, n, -1, false, false, 3, nothing, nothing, Dict())
 end
 
 State(n::Int) = State{Float64}(n)
@@ -84,7 +87,7 @@ State(n::Int) = State{Float64}(n)
 State(::Type{T}, n::Int) where T = State{T}(n)
 
 State(items::Vector{AtomState{T}}) where T = begin
-    s = State{T}(items, length(items), -1, false, false, 0)
+    s = State{T}(items, length(items), -1, false, false, 0, nothing, nothing, Dict())
 end
 State{T}() where T = State{T}(0)
 
@@ -138,5 +141,161 @@ request_i2c(s::State; all::Bool=false) = begin
     s
 end
 
+# function collectx(s::State{T}) where T
+#     x = Matrix{T}(undef, s.size, 3)
+#     collectx!(x, s)
+# end
+
+# function collectx!(x::Matrix{T}, s::State{T}) where T
+#     for i=1:s.size
+#         @. x[i,:] = s[i].t
+#     end
+#     x
+# end
 
 #endregion State
+
+
+#---------------------------------
+abstract type AbstractRepresentation end
+struct CartesianRepresentation{T} <: AbstractRepresentation
+    state::State{T}
+end
+struct InternalRepresentation{T} <: AbstractRepresentation
+    state::State{T}
+end
+
+export save, save!, restore!, AbstractRepresentation, CartesianRepresentation, InternalRepresentation
+
+# function backup(x::CartesianView{T}) where T
+#     state = x.state
+#     m = Matrix{T}(undef, state.size, 3)
+#     for i=1:state.size
+#         t = state[i].t
+#         m[i,1] = t[1]
+#         m[i,2] = t[2]
+#         m[i,3] = t[3]
+#     end
+#     m
+# end
+
+# function backup!(m::Matrix{T}, x::CartesianView{T}) where T
+#     state = x.state
+#     for i=1:state.size
+#         t = state[i].t
+#         m[i,1] = t[1]
+#         m[i,2] = t[2]
+#         m[i,3] = t[3]
+#     end
+#     m
+# end
+
+# function backup!(x::CartesianView{T}, m::Matrix{T}) where T
+#     state = x.state
+#     for i=1:state.size
+#         t = state[i].t
+#         t[1] = m[i,1]
+#         t[2] = m[i,2]
+#         t[3] = m[i,3]
+#     end
+#     x
+# end
+
+
+function save(x::InternalRepresentation{T}) where T
+    m = Matrix{T}(undef, x.state.size, 3)
+    save!(m, x)
+end
+
+function save!(m::Matrix{T}, x::InternalRepresentation{T}) where T
+    state = x.state
+    for i=1:state.size
+        s = state[i]
+        m[i,1] = s.b
+        m[i,2] = s.θ
+        m[i,3] = s.ϕ
+    end
+    m
+end
+
+function restore!(x::InternalRepresentation{T}, m::Matrix{T}) where T
+    state = x.state
+    for i=1:state.size
+        s = state[i]
+        s.b = m[i,1]
+        s.θ = m[i,2]
+        s.ϕ = m[i,3]
+    end
+    x
+end
+
+export prepare
+function prepare(x::AbstractRepresentation)
+    state = x.state
+    T = eltype(state)
+    state.x = zeros(T, state.size, 3)
+    state.f = zeros(T, state.size, 3)
+end
+
+Base.reset(s::State{T}) where T = begin
+    e = s.e
+    for k in keys(e); e[k] = 0; end
+    fill!(s.f, 0)
+    s
+end
+
+export energy,setenergy!
+energy(s::State) = sum(values(s.e))
+energy(s::AbstractRepresentation) = energy(s.state)
+setenergy!(s::State, key::Symbol, value) = (s.e[key]=value; s)
+
+
+function atmax(state::State{T}, comp::Symbol) where T
+    m = T(0)
+    idx = 0
+    x = getproperty(state, comp)
+    for i = 1:state.size
+        f = x[i,1]^2 + x[i,2]^2 + x[i,3]^2
+        if f > m
+            m = f
+            idx = i
+        end
+    end
+    return sqrt(m),idx
+end
+
+struct NamedView{T}
+    parent::State{T}
+    index::Int
+end
+
+Base.getproperty(nv::NamedView, key::Symbol) = begin
+    if key === :b
+        return nv.parent.i[nv.index,1]
+    elseif key === :θ
+        return nv.parent.i[nv.index,2]
+    elseif key === :ϕ
+        return nv.parent.i[nv.index,3]
+    elseif key === :x
+        return nv.parent.x[nv.index,1]
+    elseif key === :y
+        return nv.parent.x[nv.index,2]
+    elseif key === :z
+        return nv.parent.x[nv.index,3]
+    elseif key === :changed
+        return nv.parent.changed[nv.index]
+    end
+    getfield(nv, key)
+end
+
+# Base.setproperty!(ns::AtomState{T}, key::Symbol, val) where T = begin
+#     if key == :Δϕ
+#         setfield!(ns, :changed, true)
+#         setfield!(ns, key, T(val))
+#     elseif key == :changed
+#         setfield!(ns, :changed, val)
+#     else
+#         setfield!(ns, key, T(val))
+#     end
+#     ns
+# end
