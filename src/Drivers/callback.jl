d7c18ab5488847476b5f0f9ecc802093bdacbb04
JosePereiraUA <jose.manuel.pereira@ua.pt>
2020-11-17 16:23:50 +0000

    Callbacks now have specific frequencies

--- src/Drivers/callback.jl
+++ src/Drivers/callback.jl
@@ -1,3 +1,10 @@
 mutable struct Callback
     event::Function
+    frequency::Int
+end
+
+function (callback::Callback)(pose::Pose, driver_state::ProtoSyn.Drivers.DriverState)
+    if driver_state.step % callback.frequency == 0
+        callback.event(pose, driver_state)
+    end
 end
