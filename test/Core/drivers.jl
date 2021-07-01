49b141ac96096926bf8f87d90bda11a0c5119219
JosePereiraUA <jose.manuel.pereira@ua.pt>
2021-07-01 12:48:10 +0100

    Online documentation attempt 9

--- test/Core/drivers.jl
+++ test/Core/drivers.jl
@@ -35,87 +35,103 @@ println("-----------\n Mutators:")
         @test cb(pose, driver_state) === nothing
     end
 
-    @testset verbose = true "$(@sprintf "%-54s" "Monte Carlo")" begin
-        pose = copy(backup)
-        energy_function = ProtoSyn.Calculators.EnergyFunction([
-            ProtoSyn.Calculators.TorchANI.get_default_torchani_ensemble()
-        ])
-        m = ProtoSyn.Mutators.DihedralMutator(() -> pi, 1.0, 1.0, an"C" & rid"2")
-        t1 = ProtoSyn.Drivers.get_constant_temperature(1e10)
+    if ProtoSyn.acceleration.active === ProtoSyn.CUDA_2
+        @testset verbose = true "$(@sprintf "%-54s" "Monte Carlo")" begin
+            pose = copy(backup)
+            energy_function = ProtoSyn.Calculators.EnergyFunction([
+                ProtoSyn.Calculators.TorchANI.get_default_torchani_ensemble()
+            ])
+            m = ProtoSyn.Mutators.DihedralMutator(() -> pi, 1.0, 1.0, an"C" & rid"2")
+            t1 = ProtoSyn.Drivers.get_constant_temperature(1e10)
 
-        monte_carlo = ProtoSyn.Drivers.MonteCarlo(energy_function, m, nothing, 10, t1)
+            monte_carlo = ProtoSyn.Drivers.MonteCarlo(energy_function, m, nothing, 10, t1)
 
-        @test pose.state.e[:Total] === Inf
-        @test pose.state[pose.graph[1][2]["CA"]].Δϕ === 0.0
-        pose = monte_carlo(pose)
-        @test pose.state.e[:Total] ≈ -0.12801788747310638 atol = 1e-5
-        @test pose.state[pose.graph[1][2]["CA"]].Δϕ ≈ 31.41592653589793 atol = 1e-5
-        @test pose.state.i2c === false
-        @test pose.state.c2i === false
+            @test pose.state.e[:Total] === Inf
+            @test pose.state[pose.graph[1][2]["CA"]].Δϕ === 0.0
+            pose = monte_carlo(pose)
+            @test pose.state.e[:Total] ≈ -0.12801788747310638 atol = 1e-5
+            @test pose.state[pose.graph[1][2]["CA"]].Δϕ ≈ 31.41592653589793 atol = 1e-5
+            @test pose.state.i2c === false
+            @test pose.state.c2i === false
+        end
+    else
+        @warn "Skipping Monte Carlo test on this system: No CUDA found."
     end
 
-    @testset verbose = true "$(@sprintf "%-54s" "Steepest Descent")" begin
-        pose = copy(backup)
-        energy_function = ProtoSyn.Calculators.EnergyFunction([
-            ProtoSyn.Calculators.TorchANI.get_default_torchani_ensemble()
-        ])
+    if ProtoSyn.acceleration.active === ProtoSyn.CUDA_2
+        @testset verbose = true "$(@sprintf "%-54s" "Steepest Descent")" begin
+            pose = copy(backup)
+            energy_function = ProtoSyn.Calculators.EnergyFunction([
+                ProtoSyn.Calculators.TorchANI.get_default_torchani_ensemble()
+            ])
 
-        sd = ProtoSyn.Drivers.SteepestDescent(energy_function, nothing, 100, 0.001, 0.1)
+            sd = ProtoSyn.Drivers.SteepestDescent(energy_function, nothing, 100, 0.001, 0.1)
 
-        T = eltype(pose.state)
-        @test pose.state.e[:Total] === Inf
-        @test pose.state[pose.graph[1][2]["CA"]].Δϕ === 0.0
-        @test pose.state[pose.graph[1][2]["CA"]].ϕ === 3.141592653589793
-        @test pose.state[pose.graph[1][2]["N"]].t ≈ [3.762615121185313, -2.6664117066805666, -8.203956756092598e-7]
-        pose = sd(pose)
-        @test pose.state[pose.graph[1][2]["N"]].t[1] ≈ 3.6981331962060873 atol = 1e-5
-        @test pose.state[pose.graph[1][2]["N"]].t[2] ≈ -2.7050704999395263 atol = 1e-5
-        @test pose.state[pose.graph[1][2]["N"]].t[3] ≈ -0.04025743850412392 atol = 1e-5
-        @test pose.state.e[:Total] ≈ -0.26939642429351807 atol = 1e-5
-        @test pose.state[pose.graph[1][2]["CA"]].Δϕ === 0.0
-        @test pose.state[pose.graph[1][2]["CA"]].ϕ ≈ -3.115530980993034 atol = 1e-5
-        @test pose.state.i2c === false
-        @test pose.state.c2i === false
+            T = eltype(pose.state)
+            @test pose.state.e[:Total] === Inf
+            @test pose.state[pose.graph[1][2]["CA"]].Δϕ === 0.0
+            @test pose.state[pose.graph[1][2]["CA"]].ϕ === 3.141592653589793
+            @test pose.state[pose.graph[1][2]["N"]].t ≈ [3.762615121185313, -2.6664117066805666, -8.203956756092598e-7]
+            pose = sd(pose)
+            @test pose.state[pose.graph[1][2]["N"]].t[1] ≈ 3.6981331962060873 atol = 1e-5
+            @test pose.state[pose.graph[1][2]["N"]].t[2] ≈ -2.7050704999395263 atol = 1e-5
+            @test pose.state[pose.graph[1][2]["N"]].t[3] ≈ -0.04025743850412392 atol = 1e-5
+            @test pose.state.e[:Total] ≈ -0.26939642429351807 atol = 1e-5
+            @test pose.state[pose.graph[1][2]["CA"]].Δϕ === 0.0
+            @test pose.state[pose.graph[1][2]["CA"]].ϕ ≈ -3.115530980993034 atol = 1e-5
+            @test pose.state.i2c === false
+            @test pose.state.c2i === false
+        end
+    else
+        @warn "Skipping Steepest Descent test on this system: No CUDA found."
     end
 
-    @testset verbose = true "$(@sprintf "%-54s" "Iterated Local Search")" begin
-        pose = copy(backup)
-        energy_function = ProtoSyn.Calculators.EnergyFunction([
-            ProtoSyn.Calculators.TorchANI.get_default_torchani_ensemble()
-        ])
+    if ProtoSyn.acceleration.active === ProtoSyn.CUDA_2
+        @testset verbose = true "$(@sprintf "%-54s" "Iterated Local Search")" begin
+            pose = copy(backup)
+            energy_function = ProtoSyn.Calculators.EnergyFunction([
+                ProtoSyn.Calculators.TorchANI.get_default_torchani_ensemble()
+            ])
 
-        m = ProtoSyn.Mutators.DihedralMutator(() -> pi, 1.0, 1.0, an"C" & rid"2")
-        t1 = ProtoSyn.Drivers.get_constant_temperature(1e10)
-        monte_carlo = ProtoSyn.Drivers.MonteCarlo(energy_function, m, nothing, 10, t1)
-        ils = ProtoSyn.Drivers.ILS(energy_function, m, monte_carlo, nothing, 2, t1)
+            m = ProtoSyn.Mutators.DihedralMutator(() -> pi, 1.0, 1.0, an"C" & rid"2")
+            t1 = ProtoSyn.Drivers.get_constant_temperature(1e10)
+            monte_carlo = ProtoSyn.Drivers.MonteCarlo(energy_function, m, nothing, 10, t1)
+            ils = ProtoSyn.Drivers.ILS(energy_function, m, monte_carlo, nothing, 2, t1)
 
-        @test pose.state.e[:Total] === Inf
-        @test pose.state[pose.graph[1][2]["CA"]].Δϕ === 0.0
-        pose = ils(pose)
-        @test pose.state.e[:Total] ≈ 0.3060266077518463 atol = 1e-5
-        @test pose.state[pose.graph[1][2]["CA"]].Δϕ ≈ 65.97344572538569 atol = 1e-5
-        @test pose.state.i2c === false
-        @test pose.state.c2i === false
+            @test pose.state.e[:Total] === Inf
+            @test pose.state[pose.graph[1][2]["CA"]].Δϕ === 0.0
+            pose = ils(pose)
+            @test pose.state.e[:Total] ≈ 0.3060266077518463 atol = 1e-5
+            @test pose.state[pose.graph[1][2]["CA"]].Δϕ ≈ 65.97344572538569 atol = 1e-5
+            @test pose.state.i2c === false
+            @test pose.state.c2i === false
+        end
+    else
+        @warn "Skipping Iterated Local Search test on this system: No CUDA found."
     end
 
-    @testset verbose = true "$(@sprintf "%-54s" "Compound")" begin
-        pose = copy(backup)
-        energy_function = ProtoSyn.Calculators.EnergyFunction([
-            ProtoSyn.Calculators.TorchANI.get_default_torchani_ensemble()
-        ])
+    if ProtoSyn.acceleration.active === ProtoSyn.CUDA_2
+        @testset verbose = true "$(@sprintf "%-54s" "Compound")" begin
+            pose = copy(backup)
+            energy_function = ProtoSyn.Calculators.EnergyFunction([
+                ProtoSyn.Calculators.TorchANI.get_default_torchani_ensemble()
+            ])
 
-        m = ProtoSyn.Mutators.DihedralMutator(() -> pi, 1.0, 1.0, an"C" & rid"2")
-        t1 = ProtoSyn.Drivers.get_constant_temperature(1e10)
-        monte_carlo = ProtoSyn.Drivers.MonteCarlo(energy_function, m, nothing, 10, t1)
-        ils = ProtoSyn.Drivers.ILS(energy_function, m, monte_carlo, nothing, 2, t1)
-        cdriver = ProtoSyn.Drivers.CompoundDriver([ils, m, monte_carlo]) 
+            m = ProtoSyn.Mutators.DihedralMutator(() -> pi, 1.0, 1.0, an"C" & rid"2")
+            t1 = ProtoSyn.Drivers.get_constant_temperature(1e10)
+            monte_carlo = ProtoSyn.Drivers.MonteCarlo(energy_function, m, nothing, 10, t1)
+            ils = ProtoSyn.Drivers.ILS(energy_function, m, monte_carlo, nothing, 2, t1)
+            cdriver = ProtoSyn.Drivers.CompoundDriver([ils, m, monte_carlo]) 
 
-        @test pose.state.e[:Total] === Inf
-        @test pose.state[pose.graph[1][2]["CA"]].Δϕ === 0.0
-        pose = cdriver(pose)
-        @test pose.state.e[:Total] ≈ -0.12801788747310638 atol = 1e-5
-        @test pose.state[pose.graph[1][2]["CA"]].Δϕ ≈ 100.53096491487345 atol = 1e-5
-        @test pose.state.i2c === false
-        @test pose.state.c2i === false
+            @test pose.state.e[:Total] === Inf
+            @test pose.state[pose.graph[1][2]["CA"]].Δϕ === 0.0
+            pose = cdriver(pose)
+            @test pose.state.e[:Total] ≈ -0.12801788747310638 atol = 1e-5
+            @test pose.state[pose.graph[1][2]["CA"]].Δϕ ≈ 100.53096491487345 atol = 1e-5
+            @test pose.state.i2c === false
+            @test pose.state.c2i === false
+        end
+    else
+        @warn "Skipping Compound test on this system: No CUDA found."
     end
 end
