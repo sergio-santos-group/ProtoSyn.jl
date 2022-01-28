ee45e335148bc25c5df35a8793212e8b1f588cd5
JosePereiraUA <jose.manuel.pereira@ua.pt>
2022-01-28 10:44:29 +0000

    Removed Ramachandan potential energy function comp

--- src/Peptides/Calculators/Calculators.jl
+++ src/Peptides/Calculators/Calculators.jl
@@ -4,11 +4,6 @@ module Calculators
     using Serialization
     using Polynomials
 
-    phi_file = joinpath(Peptides.resource_dir, "ramachandran/phi-potential.jls")
-    psi_file = joinpath(Peptides.resource_dir, "ramachandran/psi-potential.jls")
-    phi_potential = open(deserialize, phi_file)
-    psi_potential = open(deserialize, psi_file)
-
     include("Caterpillar/Caterpillar.jl")
     include("restraints.jl")
     include("hydrogen_bonds.jl")
